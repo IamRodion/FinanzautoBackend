a7066b9d10e39ae7d6bf63981578e38992de045e
IamRodion <22568039+IamRodion@users.noreply.github.com>
2025-10-04 02:19:08 -0500

    Añade la primera versión de la app Añade el modelo User, el serializer, la paginación, url, views y configuraciones para funcionar

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import User
+from .serializers import UserListSerializer, UserDetailSerializer
+from .pagination import UserResultsPagination
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-registerDate')
+    permission_classes = [permissions.AllowAny]
+    pagination_class = UserResultsPagination
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return UserListSerializer
+        return UserDetailSerializer
